5f1db09e7e5a7e06faa9a5746ef9670e1834d9be
Sebastian <sebastian@MacBook-Pro-de-Sebastian.local>
2025-04-04 17:38:34 -0500

    Se agrega producto 3 para pruebas

--- Main.py
+++ Main.py
@@ -8,9 +8,12 @@ def main():
     tienda = Tienda()
     producto1 = Producto("EA", "Producto 1", "Descripción del producto 1", 10, 100.0)
     producto2 = Producto("WE", "Producto 2", "Descripción del producto 2", 5, 50.0)
+    producto3 = Producto("EA", "Producto 3", "Descripción del producto 3", 4, 30.0)
     usuario = Usuario(Carrito())
     tienda.agregar_producto_a_carrito(usuario, producto1, 2)
     tienda.agregar_producto_a_carrito(usuario, producto2, 1)
+    tienda.agregar_producto_a_carrito(usuario, producto3, 3)
+    tienda.eliminar_item_de_carrito(usuario, usuario.carrito.items[2])
     print(usuario.carrito.items)
     tienda.finalizar_compra(usuario)
 
