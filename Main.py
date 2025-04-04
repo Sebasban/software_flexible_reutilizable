5a0d4ea430f63b9d86e4b877f37a6d4198c2f54a
Sebastian <sebastian@MacBook-Pro-de-Sebastian.local>
2025-04-04 17:02:18 -0500

    Se eleimina producto 3 no usado

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ def main():
     tienda = Tienda()
     producto1 = Producto("EA", "Producto 1", "Descripción del producto 1", 10, 100.0)
     producto2 = Producto("WE", "Producto 2", "Descripción del producto 2", 5, 50.0)
-    producto3 = Producto("SP", "Producto 3", "Descripción del producto 3", 0, 25.0)
     usuario = Usuario(Carrito())
     tienda.agregar_producto_a_carrito(usuario, producto1, 2)
     tienda.agregar_producto_a_carrito(usuario, producto2, 1)
